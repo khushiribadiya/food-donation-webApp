8f8f49dd6ab3448f2136041f89ec68fda2679160
Khushi Ribadiya <khushiribadiya68@gmail.com>
2026-04-14 19:05:25 +0530

    fix mongodb connection direct

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async() => {
-	try
-	{
-		const db = process.env.MONGO_URI;
-		await mongoose.connect(db);
-		console.log("MongoDB connected...");
-	}
-	catch(err)
-	{
-		console.log(err);
-		process.exit(1);
-	}
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://mongo:CMSpaGoibaKNCFwLYWNLNwjYnIiWYuIV@monorail.proxy.rlwy.net:34982/food_donation");
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
